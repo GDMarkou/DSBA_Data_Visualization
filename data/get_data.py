b6a2bda81ba7669ccad28584c9441b25724c9399
omeurer <oscar.meurer@gmail.com>
2024-04-11 13:26:59 +0800

    adding file retrieval

---
+++ data/get_data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import requests
+import pandas as pd
+def get_data():
+    url = 'https://raw.githubusercontent.com/omeurer/road-accident-paris-data-viz/master/data/full_data.json'
+
+
+    # Fetch the JSON file from the URL
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Extract the JSON data
+        data = response.json()["results"]
+
+        # Convert JSON data to DataFrame
+        df = pd.DataFrame(data)
+
+    else:
+        print('Failed to fetch data from GitHub:', response.status_code)
+        df = None
+    return df
+
+print(get_data().head())
