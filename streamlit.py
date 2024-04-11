032cb73a3630b87228eb194de7e5b6cab3413002
George <giorgosmrkou@gmail.com>
2024-04-11 13:16:24 +0800

    Test Streamlit app

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
+
+
+st.set_page_config(
+    page_title="Paris Road Accidents",
+    page_icon="🏂",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
 
 st.write("Hello word")
